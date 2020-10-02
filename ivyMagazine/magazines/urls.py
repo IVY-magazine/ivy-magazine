5da8e7f37d59136a558bad3be6147ae4c876c553
hamlim <hamin.lim@gmail.com>
2020-10-02 20:06:08 +0900

    navbar

--- ivyMagazine/magazines/urls.py
+++ ivyMagazine/magazines/urls.py
@@ -3,5 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('magazine/', views.magazine, name="magazine")
+    path('blog/', views.blog, name="blog"),
+    path('portfolio/', views.portfolio, name='portfolio'),
+    path('magazine/', views.magazine, name="magazine"),
+    path('aboutUs/', views.aboutUs, name="aboutUs"),
+    path('contactUs/', views.contactUs, name="contactUs"),
+    path('info/', views.info, name='info'),
 ]
--- ivyMagazine/magazines/views.py
+++ ivyMagazine/magazines/views.py
@@ -5,6 +5,28 @@ def home(request):
     context = {}
     return render(request, 'magazines/homepage.html', context)
 
+def blog(request):
+    context = {}
+    return render(request, 'magazines/blog.html', context)
+
+def portfolio(request):
+    context = {}
+    return render(request, 'magazines/portfolio.html', context) 
+
 def magazine(request):
     context = {}
     return render(request, 'magazines/magazine.html', context)
+
+def aboutUs(request):
+    context = {}
+    return render(request, 'magazines/aboutUs.html', context)
+
+def contactUs(request):
+    context = {}
+    return render(request, 'magazines/contactUs.html', context)
+
+def info(request):
+    context = {}
+    return render(request, 'magazines/info.html', context)
+
+   
