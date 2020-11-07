a72cf465f756949eacf251c08d8c9acaf25bb7b1
hamlim <hamin.lim@gmail.com>
2020-11-07 16:21:48 +0900

    User Password Reset

--- ivyMagazine/magazines/urls.py
+++ ivyMagazine/magazines/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from django.conf.urls import include, url
 from django.views.generic import TemplateView
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
@@ -22,4 +23,21 @@ urlpatterns = [
     path('signUp/', views.signUp, name='signUp'),
     url(r'^mediumBlog/', TemplateView.as_view(template_name="medium.com/@ivisualizeyou"),
                    name='mediumBlog'),
+
+    path('reset_password/',
+     auth_views.PasswordResetView.as_view(template_name="magazines/password_reset.html"),
+     name="reset_password"),
+
+    path('reset_password_sent/', 
+        auth_views.PasswordResetDoneView.as_view(template_name="magazines/password_reset_sent.html"), 
+        name="password_reset_done"),
+
+    path('reset/<uidb64>/<token>/',
+     auth_views.PasswordResetConfirmView.as_view(template_name="magazines/password_reset_form.html"), 
+     name="password_reset_confirm"),
+
+    path('reset_password_complete/', 
+        auth_views.PasswordResetCompleteView.as_view(template_name="magazines/password_reset_done.html"), 
+        name="password_reset_complete"),
+
 ]
