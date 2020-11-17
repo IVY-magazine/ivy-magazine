0afcd3fe7257cd030fa44ac199e2a7bf294eda64
Seonghoon_Park <seonghoon.park@stonybrook.edu>
2020-11-17 19:04:37 +0900

    updated settings.py file to make the project adjust to azure vm settings for deployment

--- ivyMagazine/ivyMagazine/settings.py
+++ ivyMagazine/ivyMagazine/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -129,16 +130,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT= os.path.join(BASE_DIR, 'staticfiles')
+
+
 STATIC_URL = '/static/'
 
 # For any kinds of images and videos
 MEDIA_URL ='/images/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static'
+    os.path.join(BASE_DIR, 'static')
 ]
 
-MEDIA_ROOT = BASE_DIR / 'static/images'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 X_FRAME_OPTIONS = 'ALLOWALL' 
 XS_SHARING_ALLOWED_METHODS = ['POST', 'GET', 'OPTIONS', 'PUT', 'DELETE']
+
